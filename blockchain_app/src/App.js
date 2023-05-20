f7fa8ec50268c006ad42d3d284868beeb4542117
Sam Pei <sampei7511@gmail.com>
2023-05-19 22:32:37 -0400

    First commit for front end

---
+++ blockchain_app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from "./Components/NavBar"
+import './App.css'
+
+function App() {
+  return  (
+    <body>
+      <NavBar/>
+    </body>
+    )
+}
+
+export default App
