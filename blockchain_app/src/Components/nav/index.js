05d8a59dc133ba477bb418231524d9494c782dec
Christian <cflorea@uwaterloo.ca>
2023-05-20 10:11:23 -0400

    Update

--- blockchain_app/src/Components/nav/index.js
+++ blockchain_app/src/Components/nav/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-// import "./nav.css"
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar" style={{width:"100%"}}>
       <ul className="navbar-list">
         <li className="navbar-item"> 
             <NavLink 
--- blockchain_app/src/Components/people/index.js
+++ blockchain_app/src/Components/people/index.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
 import { Title } from "../title";
 import { BsFillPeopleFill as Icon} from "react-icons/bs";
 import Table from "./table";
+import "./people.css"
+
 
 function People() {
-    const data = [
-        { id: 1, name: 'John Doe', title: "CEO", department: "Engineering", email: 'john@example.com' },
-        { id: 2, name: 'John Doe', title: "CEO", department: "Engineering", email: 'john@example.com' },
-        { id: 3, name: 'John Doe', title: "CEO", department: "Engineering", email: 'john@example.com' },
-      ];
+    const [popupOpen, setPopupOpen] = useState(false)
+    const [data, setData] = useState([])
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        loadTable()
+    }, [])
+    
+    const handlePopupClick = () => {
+        setPopupOpen(!popupOpen)
+    }
+
+    const loadTable = () => {
+        setData([
+            { id: 1, name: 'John Doe', title: "CEO", department: "Engineering", email: 'john@example.com' },
+            { id: 2, name: 'John Doe', title: "CEO", department: "Engineering", email: 'john@example.com' },
+            { id: 3, name: 'John Doe', title: "CEO", department: "Engineering", email: 'john@example.com' },
+          ])
+        setLoaded(true)
+    }
     return(
         <section>
             <Title title={"People"} icon={Icon}/>
+            <Button 
+            style={{margin:"10px"}}
+            onClick={handlePopupClick}> {popupOpen ? "Close" : "Add Employee"}</Button>
+            {popupOpen && 
+            <div>
+                Data
+            </div>
+            }
+            {loaded ? 
             <div className="container">
-        <Table datas={data}/>
-        </div>
-    </section>
+                <Table datas={data}/>
+            </div> : <p>Loading...</p>}
+        </section>
     );
 }
 
